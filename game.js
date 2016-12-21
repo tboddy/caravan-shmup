85ea8daee035208e4c443155468b5e4d8ce7fab5
Trevor Boddy <trevor@truefire.com>
2016-12-21 10:30:51 -0500

    remove borked logo

--- game.js
+++ game.js
@@ -160,12 +160,12 @@ studiosLogoImg.src = 'img/studioslogo.png';
 var drawStart = function(){
 	var verString = 'pre alpha 0.02',
 		startString = 'press b3 or enter',
-		creditString = '2016';
+		creditString = '2016 t.boddy';
 	context.drawImage(startLogoImg, (gameWidth / 2) - 64, grid * 2);
 	drawString(verString, textCenter(verString), grid * 9);
 	drawString(creditString, textCenter(creditString), grid * 10);
-	drawString(startString, textCenter(startString), grid * 11);
-	context.drawImage(studiosLogoImg, (gameWidth / 2) - 32, grid * 12);
+	drawString(startString, textCenter(startString), grid * 12);
+	// context.drawImage(studiosLogoImg, (gameWidth / 2) - 32, grid * 12);
 };
 
 var textCenter = function(string){
